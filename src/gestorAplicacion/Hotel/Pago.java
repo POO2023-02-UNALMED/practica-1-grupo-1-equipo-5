8c2c537be22294461cc0b3ab525c8cceed02a084
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-22 11:56:29 -0500

    Reconsideración de clase Pago

--- src/gestorAplicacion/Hotel/Pago.java
+++ src/gestorAplicacion/Hotel/Pago.java
@@ -3,38 +3,21 @@ import java.util.ArrayList;
 
 public class Pago {
 	
-    private double totalPago;
+    private Integer total;
+    private Servicio tipo_servicio;
+    private String fecha_pago;
     
-    private String fechaPago;
-    private static ArrayList<Servicio> servicios = new ArrayList<Servicio>();
-    private ArrayList<Reserva> reservas= new ArrayList<Reserva>();
-    
-    
-    public Pago(double totalPago, String fechaPago) {
-        this.totalPago = totalPago;
-        this.fechaPago = fechaPago;
-    }
-    
-    public void setFechaPago(String fechaPago) {this.fechaPago = fechaPago;}
-    public String getFechaPago() {return fechaPago;}
-    public void setTotalPago(double totalPago) {this.totalPago = totalPago;}
-    public double getTotalPago() {return totalPago;}
-    
-    public  void descontar(int totalReserva) {
-    	
-    	setTotalPago(totalPago-totalReserva);
+    public Pago() {
     	
     }
-
-    public static void agregarServicio(Servicio servicio) {
-        servicios.add(servicio);
+    
+    public Pago(Integer total, Servicio tipo_servicio, String fecha_pago) {
+    	this.total = total;
+    	this.tipo_servicio= tipo_servicio;
+    	this.fecha_pago = fecha_pago;
+    	
     }
-
-    public int sumarTotal() {
-        int total = 0;
-        for (Servicio servicio : servicios) {
-            total += servicio.getPrecio();
-        }
-        return total;
-    }
-}
+    
+    
+    
+    
