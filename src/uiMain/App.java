9feaf1e96f2ccaf24e251349d3f565aea885011b
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-23 08:48:25 -0500

    Update App.java

--- src/uiMain/App.java
+++ src/uiMain/App.java
@@ -21,6 +21,30 @@ public class App {
         int opcion;
         Cliente client;
         Boolean pago = false;
-	
+        // TODO: Datos de prueba
+        Servicio serv1 = new Servicio("Desayuno", 2.99, 5, "01-01-2023", "01-20-2023");
+        Servicio serv2 = new Servicio("Actividad tematica", 6.99, 4, "01-30-2023", "02-05-2023");
+
+        //habitaciones.add(habi1);
+        //habitaciones.add(habi2);
+        servicios.add(serv1);
+        servicios.add(serv2);
+        Hotel hotelObj = new Hotel();
+        hotelObj.setNombre("Sol caribe");
+        hotelObj.setDireccion("<direccion>");
+        hotelObj.setComentarios(new String[]{"Excelente", "Hola"});
+        Hotel hotel02 = new Hotel("OnVcation", "San Luis");
+        hotel02.setComentarios(new String[]{"Excelente", "Hola"});
+        //hotelObj.setHabitaciones(habitaciones);
+        hotelObj.setServicios(servicios);
+        hotelObj.addHabitaciones(new Habitacion(hotelObj,"101", 1, 200000.0, TipoHabitacion.INDIVIDUAL, false));
+        hotelObj.addHabitaciones(new Habitacion(hotelObj, "102", 2, 200000.0, TipoHabitacion.DOBLE, true));
+        hotelObj.addHabitaciones(new Habitacion(hotelObj, "103", 2, 200000.0, TipoHabitacion.DOBLE, false));
+        hotelObj.addHabitaciones(new Habitacion(hotelObj,"104", 2, 200000.0, TipoHabitacion.MATRIMONIAL, true));
+        hotelObj.addHabitaciones(new Habitacion(hotelObj, "105", 1, 200000.0, TipoHabitacion.INDIVIDUAL, true));
+        hotelObj.addHabitaciones(new Habitacion(hotelObj, "106", 2, 200000.0, TipoHabitacion.DOBLE, false));
+        hotelObj.addHabitaciones(new Habitacion(hotelObj,"107", 3, 200000.0, TipoHabitacion.SUITE, true));
+        hotelObj.addHabitaciones(new Habitacion(hotelObj,"108", 2, 200000.0, TipoHabitacion.DOBLE, true));
+
 
 }
