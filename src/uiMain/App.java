6464089e8d85ba188f8ca020c177ebba501a94d2
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-23 08:37:03 -0500

    inicio de la uiMain

--- src/uiMain/App.java
+++ src/uiMain/App.java
@@ -1,5 +1,26 @@
-//aquí va el main, donde se ejecutará la interacción con el usuario
-//através de consola
+package gestorAplicacion.uiMain;
+
+import gestorAplicacion.personas.*;
+import gestorAplicacion.hotel.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
 public class App {
+	
+    static List<Hotel> hoteles = Hotel.getHoteles();
+    public static List<Servicio> servicios = new LinkedList<>();
+    public static List<Habitacion> habitaciones = new LinkedList<>();
+    
+ // Inicializacion de la App
+    public static void initApp() {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        Cliente client;
+        Boolean pago = false;
+	
 
 }
