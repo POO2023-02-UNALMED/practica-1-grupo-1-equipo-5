fdc53ddc3afc747501e4b12b8ee2bbb4c8f37b4a
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-24 02:01:41 -0500

    Update GestionReserva.java

--- src/gestorAplicacion/Hotel/GestionReserva.java
+++ src/gestorAplicacion/Hotel/GestionReserva.java
@@ -1,77 +1,110 @@
 package gestorAplicacion.Hotel;
 import gestorAplicacion.personas.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class GestionReserva {
-	    private Cliente cliente;
-	    private Habitacion habitacion;
-	    private Pago factura;
-	    private String ingreso;
-	    private String salida;
-	    private List<Servicio> serviciosAdicionales;
+public class GestionReserva implements Serializable {
+    public static List<GestionReserva> reservas = new ArrayList<>();
+    private Cliente cliente;
+    private Habitacion habitacion;
+    private Pago factura;
+    private int nochesXestadia;
 
-	    public void aggReserva(Habitacion habitacion){
-	        this.habitacion = habitacion;
-	    }
-	    public void cancelarReserva(){
-	        //Reserva
-	    }
-	    //HACER FUNCION
-	    public double cotizar(){
-	        return 3.1416d;
-	    }
+    public List<Servicio> serviciosAdicionales = new ArrayList<>();
 
-	    public void aggServicio(Servicio servicio){
-	        this.serviciosAdicionales.add(servicio);
-	    }
-	    
-	    public Cliente getCliente(){
-	        return cliente;
-	    }
+    private Boolean estado;
+    private Hotel hotel;
 
-	    public Habitacion getHabitacion(){
-	        return habitacion;
-	    }
+    public GestionReserva(Cliente cliente, Habitacion habitacion, int nochesXestadia, Boolean estado, Hotel hotel) {
+        this.cliente = cliente;
+        this.habitacion = habitacion;
+        this.nochesXestadia = nochesXestadia;
+        this.estado = estado;
+        this.hotel = hotel;
+        reservas.add(this);
+    }
 
-	    public String getIngreso() {
-	        return ingreso;
-	    }
+    public Hotel getHotel() {
+        return hotel;
+    }
 
+    public void aggReserva(Habitacion habitacion){
+        this.habitacion = habitacion;
+    }
+    public void cancelarReserva(){
+        //Reserva
+    }
+    //HACER FUNCION
+    public double cotizar(int[] servicios){
+        double precioCotizacion = 0;
+        if(Servicio.getServicios().isEmpty()){
+            return 0d;
+        }
+        for (int i = 0; i < servicios.length; i++) {
+            precioCotizacion += Servicio.getServicios().get(servicios[i]).getPrecio();
+        }
+        return precioCotizacion;
 
-	    public void setIngreso(String ingreso) {
-	        this.ingreso = ingreso;
-	    }
+    }
 
-	    public String getSalida() {
-	        return salida;
-	    }
+    public void aggServicio(Servicio servicio){
+        this.serviciosAdicionales.add(servicio);
+    }
 
-	    public void setSalida(String salida) {
-	        this.salida = salida;
-	    }
+    public Integer getNochesXEstadia() {
+        return this.nochesXestadia;
+    }
 
-	    public List<Servicio> getServiciosAdicionales() {
-	        return serviciosAdicionales;
-	    }
+    public Cliente getCliente(){
+        return cliente;
+    }
 
-	    public void setServiciosAdicionales(List<Servicio> serviciosAdicionales) {
-	        this.serviciosAdicionales = serviciosAdicionales;
-	    }
+    public Habitacion getHabitacion(){
+        return habitacion;
+    }
 
-	    //Hacer la de ver factura
-	    //public Pago verFactura(){
-	      //  factura.toString();
-	    //}
+    //public String getIngreso() {return ingreso;}
 
-	    @Override
-	    public String toString() {
-	        return "GestionReserva{" +
-	                "cliente=" + cliente.toString() +
-	                ", habitacion=" + habitacion +
-	                ", ingreso='" + ingreso + '\'' +
-	                ", salida='" + salida + '\'' +
-	                ", serviciosAdicionales=" + serviciosAdicionales +
-	                "}";
-	    }
-	}
-	
+    //public void setIngreso(String ingreso) {this.ingreso = ingreso;}
+
+    //public String getSalida() {return salida;}
+
+    //public void setSalida(String salida) {this.salida = salida;}
+
+    public List<Servicio> getServiciosAdicionales() {
+        return serviciosAdicionales;
+    }
+
+    public void setServiciosAdicionales(List<Servicio> serviciosAdicionales) {
+        this.serviciosAdicionales = serviciosAdicionales;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    //Hacer la de ver factura
+    //public Pago verFactura(){
+      //  factura.toString();
+    //}
+
+public void borrarReserva(){
+        GestionReserva.reservas.remove(this);
+}
+
+    @Override
+    public String toString() {
+        return "Reservas" +'\n' +
+                "cliente:" + cliente.toString() +'\n' +
+                "habitacion:" + habitacion +'\n' +
+                "dias reservados: " + nochesXestadia +'\n' +
+                "serviciosAdicionales:" + serviciosAdicionales +'\n'+
+                "-----------------------------------------------------";
+    }
+}	
