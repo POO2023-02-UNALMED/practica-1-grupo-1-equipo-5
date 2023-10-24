c834d91ea0d2a657e8bc8bac8298d2ce3e98c8df
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-23 22:01:23 -0500

    reconstruyendo empleado

--- src/gestorAplicacion/personas/Empleado.java
+++ src/gestorAplicacion/personas/Empleado.java
@@ -2,73 +2,32 @@ package gestorAplicacion.personas;
 import gestorAplicacion.Hotel.*;
 import java.util.ArrayList;
 
-public class Empleado extends Persona {
-    //Atributos especificos de los empleados
-    private long IDempleado;
-    private ROL cargo;
-    private Hotel hotel;
-    private double sueldo;
-    private enum ROL {RECEPCIONISTA, GERENTE, CONSERJE, MANTENIMIENTO, SEGURIDAD, ENTRENADOR, MASAJISTA}
-    private ArrayList<Servicio> listServicio = new ArrayList<>();
+public class Empleado extends Persona{
+    private String IDEmpleado;
+    private String rol;
+    private Double sueldo;
 
-    //constructores---------------------------------------------------------------------
-    public Empleado() {super();}
-    public Empleado(String nombre, int edad, TPD tipoDocumento, long cedula, char sexo, long IDempleado, Hotel hotel, double sueldo, ROL cargo) {
-        super(nombre, edad, tipoDocumento, cedula, sexo);
-        this.IDempleado = IDempleado;
+    public Empleado(){
+
+    }
+
+    public Empleado(String nombre, Integer edad, Character sexo, String tipo_documento, String num_documento, String telefono, String direccion, String correo, Integer calificacion, String IDEmpleado, String rol, Double sueldo) {
+        super(nombre, edad, sexo, tipo_documento, num_documento, telefono, direccion, correo);
+        this.IDEmpleado = IDEmpleado;
+        this.rol = rol;
         this.sueldo = sueldo;
-        this.cargo = cargo;
-        this.hotel = hotel;
     }
 
-
-    // set y get
-    public void setCargo(ROL cargo) {this.cargo = cargo;}
-
-    public ROL getCargo() {return cargo;}
-
-    public void setSueldo(double sueldo) {this.sueldo = sueldo;}
-
-    public double getSueldo() {return sueldo;}
-
-    public void setHotel(Hotel hotel) {this.hotel = hotel;}
-
-    public Hotel getHotel() {return hotel;}
-
-    public void setServicio(Servicio s) {
-        listServicio.add(s);
-        s.addEmpleado(this);
+    public String getIDEmpleado() {
+        return IDEmpleado;
     }
 
-    public ArrayList<Servicio> getServicio() {return listServicio;}
-
-    public void setIDempleado(long IDempleado) {this.IDempleado = IDempleado;}
-
-    public long getIDempleado() {return IDempleado;}
-
-
-    //--------------------------------------------------------------------
-
-    //usando el metodo abstracto de la clase padre Persona
-
-    @Override
-    public void ascender() {
-        //logica implicada en el ascenso
+    public void setIDEmpleado(String IDEmpleado) {
+        this.IDEmpleado = IDEmpleado;
     }
 
-    @Override
-    public String personaRol() {
-        return "Empleado "+cargo+"  en el Hotel"+hotel;
+    public String getRol() {
+        return rol;
     }
 
-    @Override
-    public String toString() {
-        return "Nombre: " + nombre + "\n" +
-                "Edad: " + edad + "\n" +
-                "Cédula: " + num_documento + "\n" + // num_documento es un atributo heredado de Persona
-                "Sexo: " + sexo + "\n" +
-                "ID Empleado: " + IDempleado + "\n" +
-                "Rol: " + cargo + "\n" +
-                "Hotel: " + hotel.getNombre(); // Suponiendo que Hotel tiene un método getNombre()
-    }
-}
+    
